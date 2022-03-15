4447a9e2d9918db95184a01383a8d96e2cfb0f17
Reza Hesami <rezahesami10@gmail.com>
2022-03-15 20:53:45 +0330

    nothing

--- test.js
+++ test.js
@@ -49,7 +49,7 @@ async function main() {
   for (let z = startZ; z <= endZ; z++)
     for (let x = startX; x <= endX; x++)
         await rxjs.firstValueFrom(UtopiaApi.placeBlock(Inputs.blockType, x, endY, z));
-  test("hello");
+  test();
   // for (let x = startX; x <= endX; x++) {
   //   for (let y = startY; y <= endY; y++) {
   //     for (let z = startZ; z <= endZ; z++) {
